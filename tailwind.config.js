81f996e7baa91d59f46eafe2b67d05e39bfdabaa
Joseph Crowell <joseph.w.crowell@gmail.com>
2023-08-20 08:41:39 +1000

    add heading sizing to css

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,6 +13,10 @@ module.exports = {
     ],
     theme: {
         extend: {
+            boxShadow: {
+                cta: '0 20px 15px -15px rgba(16, 49, 65, 0.15)',
+                button: '0 10px 20px -10px rgba(47, 46, 45, 0.25)',
+            },
             colors: {
                 primary: 'var(--primary)',
                 secondary: 'var(--secondary)',
@@ -42,6 +46,37 @@ module.exports = {
                 fuchsia: colors.fuchsia,
                 pink: colors.pink,
                 rose: colors.rose,
+            },
+            fontFamily: {
+                standard: ['Inter', 'sans-serif'],
+                heading: ['Yantramanav', 'sans-serif'],
+                code: ['Cousine', 'monospace'],
+            },
+            fontSize: {
+                base: ['15.5px', '26px'],
+                'base-mobile': ['14.5px', '23px'],
+                'top-nav': ['13px', '18px'],
+                'main-nav': ['18px', '24px'],
+                heading: ['56px', '62px'],
+                h1: ['44px', '48px'],
+                h2: ['36px', '40px'],
+                h3: ['28px', '34px'],
+                h4: ['22px', '26px'],
+                h5: ['18px', '24px'],
+                h6: ['16px', '22px'],
+                caption: ['14px', '22px'],
+                'caption-mobile': ['13px', '20px'],
+                disclaimer: ['13px', '20px'],
+                'disclaimer-mobile': ['12px', '18px'],
+                code: ['16px', '24px'],
+                'code-mobile': ['15px', '22px'],
+            },
+            letterSpacing: {
+                tighter: '-.5px',
+                tight: '-.25px',
+                normal: '0',
+                wide: '.25px',
+                wider: '.5px',
             },
         },
     },
